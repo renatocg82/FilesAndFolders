a79ce8da921fc8ded19fe3a7f7be0b96c62927aa
renatocg82 <66907169+renatocg82@users.noreply.github.com>
2023-07-23 12:46:14 -0300

    First commit

---
+++ src/application/ReadingAndWriting.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ReadingAndWriting {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner scString = new Scanner(System.in);
+		Scanner sc = new Scanner (System.in);
+		
+
+		
+		
+		
+		scString.close();
+		sc.close();
+		
+	}
+
+}
